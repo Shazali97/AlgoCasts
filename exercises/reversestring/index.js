bebf6834b9d07170cb18f194c95f0614f1c2adc9
Shaz <shaz.ali1997@gmail.com>
2020-03-05 20:05:01 -0600

    reversestring solutions and debug notes

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// 2nd Solution
+function reverse(str) {
+    // initiate empty str
+    let newStr = "";
+    // new for loop syntax iterate over each
+    for (let character of str) {
+        // put the next character ahead of the existing str 
+        newStr = character + newStr
+    }
+    return newStr
+}
 
 module.exports = reverse;
+
+// 1st Solution 
+// function reverse(str) {
+//     return str.split("").reverse().join("")
+// }
+
+
+// 3rd Solution
+// function reverse(str) {
+//     return str.split("").reduce((rev, char) => char + rev, "")
+// }
+
+// debugger;
+// node inspect file.js
+// 'c' to get to debugger
+// 'repl' to check values
+// exit and 'c' again to iterate over
