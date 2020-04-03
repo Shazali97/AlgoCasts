8297f56f606a203bf800f75038496edbd3e27219
Shaz <shaz.ali1997@gmail.com>
2020-04-03 17:40:08 -0500

    chunk, fizzbuzz, and maxchars solutions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const arr = str.split("")
+    const chars = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let characters of arr) { 
+        if (!chars[characters]) {
+            chars[characters] = 1;
+        } else {
+            chars[characters]++;
+        }
+        // chars[characters] = chars[characters] + 1 || 1
+    }
+
+    for (let characters in chars) {
+        if (chars[characters] > max) {
+            max = chars[characters]
+            maxChar = characters
+        }
+    }
+
+    return maxChar
+}
+
+maxChar("Hello there!");
 
 module.exports = maxChar;
