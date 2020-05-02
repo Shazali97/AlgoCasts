465c70a3ffc6a93df51c95b443aeb826e5f1a39d
Shaz <shaz.ali1997@gmail.com>
2020-05-02 15:56:28 -0500

    queue and memoization

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -4,10 +4,25 @@
 // Adding to the queue should store an element until
 // it is removed
 // --- Examples
-//     const q = new Queue();
+//     const q = new Queue(); // <<< 'new instance'
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    // initialize an array and assign it
+    constructor() { // called when new instance is created
+        this.data = [];
+    }
+
+    add(record) {
+        // adds to the front of array
+        this.data.unshift(record);
+    }
+
+    remove() {
+        // removes from the end of array
+        return this.data.pop();
+    }
+}
 
 module.exports = Queue;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,22 +17,32 @@
 //       '### '
 //       '####'
 
-function steps(n, row = 0, stair = '') {
-    if (n === row) {
-        return;
-    }
-    
-    if (n === stair.length) {
-        console.log(stair);
-        return steps(n, row + 1);
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
     }
 
-    stair.length <= row ? stair += '#' : stair += ' ';
-
-    steps(n, row, stair);
-}   
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
+
+/*
+    Runtime Complexity:
+
+    As 'n' increases by 1, we have to do (n*n) things
+
+    'n^2' or 'quadratic' runtime
+
+*/
 
 // My Solution
 // function steps(n) {
@@ -41,4 +51,20 @@ module.exports = steps;
 //     for (i = 1; i <= n; i++) {
 //         console.log(hash.repeat(i) + space.repeat(i - 1));
 //     }
+// }
+
+// Recursive Solution
+// function steps(n, row = 0, stair = '') {
+//     if (n === row) {
+//         return;
+//     }
+
+//     if (n === stair.length) {
+//         console.log(stair);
+//         return steps(n, row + 1);
+//     }
+
+//     stair.length <= row ? stair += '#' : stair += ' ';
+
+//     steps(n, row, stair);
 // }
